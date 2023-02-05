a15db568aa688cb26481ba111f335e81ae49c6af
Albert <playalbert40@gmail.com>
2023-02-05 17:59:16 +0200

    Add product design update

--- Shopville/main/forms.py
+++ Shopville/main/forms.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput, CharField
 from .models import Goods
 
 
 class GoodsForm(ModelForm):
+    item_name = CharField(empty_value='', widget=TextInput(attrs={'placeholder': 'Product name'}))
+    item_description =  CharField(empty_value='', widget=Textarea(attrs={'placeholder': 'Description'}))
+
     class Meta:
         model = Goods
         fields = ['item_image', 'item_name', 'item_description', 'price', 'sellers_email']
